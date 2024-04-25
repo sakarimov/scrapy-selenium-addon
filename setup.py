9fb56fa1f48b0be574b66946e88916b370fb9ab9
jogobeny <79165095+jogobeny@users.noreply.github.com>
2024-04-25 22:38:18 +0200

    Remove usage of the internal `pip`

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-"""This module contains the packaging routine for the pybook package"""
-
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
 
 
 def get_requirements(source):
@@ -20,15 +10,13 @@ def get_requirements(source):
         The filename containing the requirements
 
     """
+    with open(source, "r") as f:
+        requirements = f.read().splitlines()
 
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
+    return requirements
 
 
 setup(
     packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
+    install_requires=get_requirements("requirements/requirements.txt")
 )
-
-
